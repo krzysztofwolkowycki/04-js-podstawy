e1876669d32d484f58c53509a30b212574c8206d
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2019-03-30 12:12:47 +0100

    var/let/const

---
+++ js/02-zmienne-stale.js
@@ -0,0 +1,16 @@
+console.group('Zmienne');
+var firstName = 'Jan';
+
+console.log(firstName);
+
+firstName = 'Andrzej';
+console.log(firstName);
+
+let lastName = 'Nowak';
+
+console.log(lastName);
+
+lastName = 'Kowalski';
+console.log(lastName);
+
+console.groupEnd();

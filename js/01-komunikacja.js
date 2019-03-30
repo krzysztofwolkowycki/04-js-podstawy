db1f36cc3b9018a5911b9e58d033781a5350dda8
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2019-03-30 11:33:14 +0100

    console.log-console.group

--- js/01-komunikacja.js
+++ js/01-komunikacja.js
@@ -1,3 +1,21 @@
-alert('To jest komunikat alert');
-confirm('To jest komunika okienka confirm');
-prompt('To jest komunikat okienka prompt');
+//Okienka dialogowe
+// alert('To jest komunikat alert');
+// confirm('To jest komunika okienka confirm');
+// prompt('To jest komunikat okienka prompt');
+
+//Komunikaty do konsoli
+console.group('Grupa 1');
+console.log('TO jest komunikat 1');
+console.log('To jest komunikat 2');
+
+console.group('Podgrupa A');
+console.log('To jest komunikat 3');
+console.log('To jest komunikat 4');
+console.groupEnd();
+
+console.log('To jest komunikat 5');
+console.groupEnd();
+
+console.log('To jest komunikat 3');
+
+

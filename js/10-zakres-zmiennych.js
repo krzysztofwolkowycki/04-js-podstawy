e8a7bbbc6c01e1da9766a4d088a2d68124489f65
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2019-04-12 21:01:30 +0200

    var; let

---
+++ js/10-zakres-zmiennych.js
@@ -0,0 +1,22 @@
+function test (){
+    function test2 (){
+        b = 20;
+        console.log(a);
+        console.log(b);
+    }
+    
+    console.log(a);
+    console.log(b);
+
+    test2();
+}
+if (true){
+    let a = 10;
+    let b = 30;
+}
+var a = 10;
+var b = 30;
+
+console.log(a);
+test();
+console.log(b);

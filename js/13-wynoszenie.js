0ed7af360070682ce3efdfe0fea2aabcc29e8870
Krzysztof Wolkowycki <wolkowycki.k@gmail.com>
2019-04-13 10:40:58 +0200

    Wynoszenia

---
+++ js/13-wynoszenie.js
@@ -0,0 +1,20 @@
+console.log(a);
+
+
+var a = 'To jest a';
+
+console.log(a);
+
+test();
+function test (){
+    console.log('To jest funkcja test');
+}
+
+test();
+
+alfa();
+var alfa = function () {
+    console.log('To jest funkcja alfa');
+};
+
+alfa();
